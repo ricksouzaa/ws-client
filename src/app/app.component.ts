336a05ce9bc7cf994fe3cd3a74acd8d73d9bbecb
Ricardo Antonio de Souza <rick.souzaa@gmail.com>
2023-01-17 16:58:27 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Manager } from 'socket.io-client';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ws-client';
+
+  private nsp1: any;
+  private nsp2: any;
+
+  constructor() {
+    const ws = new Manager('http://localhost:3000');
+    this.nsp1 = ws.socket('/nsp1')
+      .on('connect', () => {
+        console.log(`connected on: nsp1`);
+      }).on('disconnect', () => {
+        console.log(`disconnected on: nsp2`);
+      });
+
+    this.nsp2 = ws.socket('/nsp2');
+    this.nsp2.on('connect', () => {
+      console.log(`connected on: nsp2`);
+    });
+    this.nsp2.on('disconnect', () => {
+      console.log(`disconnected on: nsp2`);
+    });
+    this.join();
+  }
+
+  join() {
+    // const message = this.buildMessage('datweb', 'teste');
+    // this.ws.nsp = '/delivery';
+    // this.ws.on('connect', () => {
+    // this.ws.emit('join', message);
+    // });
+    this.nsp1.on('datweb', (data: any) => console.table(data));
+    this.nsp2.on('datwebx', (data: any) => console.table(data));
+  }
+
+  sendNsp1() {
+    this.nsp1.emit('status', this.buildMessage('datweb', 'mudou o status'));
+  }
+
+  sendNsp2() {
+    this.nsp2.emit('status', this.buildMessage('datweb', 'mudou o status'));
+  }
+
+  buildMessage(domain: string, status: string) {
+    return {
+      domain,
+      order: 100,
+      status
+    };
+  }
+}
+
+export interface Message {
+  domain: string;
+  order: number;
+  status: string;
 }
